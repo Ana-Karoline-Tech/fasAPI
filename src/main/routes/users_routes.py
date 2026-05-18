cb5766b9bfd71d0de628247c463f60d3387564ac
Ana-Karoline-Tech <anna.karolinawork@gmail.com>
2026-05-18 20:24:01 -0300

    feat: Implementingcriar criar_usuário and buscar_usuarios_por_nome routes with composer and validadtors

--- src/main/routes/users_routes.py
+++ src/main/routes/users_routes.py
@@ -1,12 +1,33 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from src.views.http_types.http_request import HttpRequest
+from src.validators.user_register_validator import UserInput
+
+from src.main.composer.user_finder_composer import user_finder_composer
+from src.main.composer.user_register_composer import user_register_composer
 
 users_routes = APIRouter(tags=["Usuários"])
 
 @users_routes.post("/users")
-async def crear_usuario():
+async def criar_usuario(body: UserInput):
+       http_request = HttpRequest(body=dict(body))
+       user_register = user_register_composer ()
 
-        return JSONResponse(
-                content={"Ola": "Mundo"}, 
-                status_code=200
+       http_response = await user_register.handle_register_user(http_request)
+       
+       return JSONResponse(
+                content=http_response.body,
+                status_code=http_response.status_code
                 )
+
+@users_routes.get("/users/{user_name}")
+async def buscar_usuarios_por_nome(user_name: str):
+       http_request = HttpRequest(path_params={"user_name": user_name})
+       user_finder = user_finder_composer()
+
+       http_response = await user_finder.handle_find_user_by_name(http_request)
+
+       return JSONResponse(
+               content=http_response.body,
+               status_code=http_response.status_code
+        )
